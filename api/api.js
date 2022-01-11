4f11ee01231f4a4c1dfd8782cd6962b13abfa0d6
Deron McGuire <Deronmcguire23@yahoo.com>
2022-01-11 16:04:01 -0600

    Added Series table and series get handler

--- api/api.js
+++ api/api.js
@@ -1,10 +1,14 @@
 const express = require("express");
 
 const artistRouter = require("./artists")
+const seriesRouter = require("./series")
 
 const apiRouter = express.Router();
 
 // api/artists/ middleware
 apiRouter.use("/artists", artistRouter);
+
+// api/series/ middleware
+apiRouter.use("/series", seriesRouter)
 
 module.exports = apiRouter
